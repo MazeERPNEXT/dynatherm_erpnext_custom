f0965b6a45be5f80fea718739f0f8b46672b794e
Pandiyarajan <msk312508@gmail.com>
2025-09-24 14:51:33 +0530

    Dimension in mq & rfq

--- erp_custom/public/js/item.js
+++ erp_custom/public/js/item.js
@@ -1,29 +1,3 @@
-// frappe.ui.form.on('Item', {
-//     length: function(frm) {
-//         calculate_kg(frm);
-//     },
-//     width: function(frm) {
-//         calculate_kg(frm);
-//     },
-//     thickness: function(frm) {
-//         calculate_kg(frm);
-//     }
-// });
-
-// function calculate_kg(frm) {
-//     if (frm.doc.length && frm.doc.width && frm.doc.thickness) {
-//         const density = 7.85; 
-//         let value = frm.doc.length * frm.doc.width * frm.doc.thickness * density;
-//         let value_in_millions = (value / 1000000).toFixed(2);
-
-//         frm.set_value("kilogramskgs", value_in_millions);
-//     }
-//     else{
-//         frm.set_value("kilogramskgs", "0");
-//     }
-// }
-
-
 frappe.ui.form.on('Item', {
     validate: function(frm) {
         if (frm.doc.custom_length && frm.doc.custom_width && frm.doc.custom_thickness) {
