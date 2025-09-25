0fee48645a14adc0e98062aea9814e51ed92ce11
Pandiyarajan <msk312508@gmail.com>
2025-09-25 17:54:22 +0530

    Item & Document Details

--- erp_custom/public/js/item.js
+++ erp_custom/public/js/item.js
@@ -1,15 +1,12 @@
+
 frappe.ui.form.on('Item', {
     validate: function(frm) {
         if (frm.doc.custom_length && frm.doc.custom_width && frm.doc.custom_thickness) {
-            const density = 7.85; 
-
-            let grams = frm.doc.custom_length * frm.doc.custom_width * frm.doc.custom_thickness * density;
-            let kilograms = (grams / 1000).toFixed(2);
-
-            frm.set_value("custom_kilogramskgs", kilograms);
+            const density = 7.85;
+            let weight = (frm.doc.custom_length * frm.doc.custom_width * frm.doc.custom_thickness * density) / 1000000;
+            frm.set_value("custom_kilogramskgs", weight.toFixed(2));
         } else {
             frm.set_value("custom_kilogramskgs", 0);
         }
     }
 });
-
