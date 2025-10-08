ab08b3c4945bf3900c9130c23bc983be198428fe
Mazeworks <msk312508@gmail.com>
2025-10-08 12:52:51 +0530

    CRFQ & Estimates

---
+++ erp_custom/erp_custom/doctype/estimate_item/estimate_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, maze and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EstimateItem(Document):
+	pass
