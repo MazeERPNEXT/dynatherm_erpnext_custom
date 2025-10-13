f378c1f85bfe284d58240d278198d3debc0927bf
Mazeworks <msk312508@gmail.com>
2025-10-13 13:36:38 +0530

    customer & supplier extra fields

---
+++ erp_custom/erp_custom/doctype/certification_type/certification_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, maze and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Certification Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
