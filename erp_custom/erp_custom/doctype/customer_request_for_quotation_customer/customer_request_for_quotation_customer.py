3f36c147b4bed8d697ddc9338529016f77be81a2
Mazeworks <msk312508@gmail.com>
2025-10-17 15:24:10 +0530

    CRFQ doctype

--- erp_custom/erp_custom/doctype/customer_request_for_quotation_customer/customer_request_for_quotation_customer.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, maze and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class CustomerRequestForQuotationCustomer(Document):
-	pass
