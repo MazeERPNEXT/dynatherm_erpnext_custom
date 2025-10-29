fc6f8cea819e11b9b1a8a5d87406424b7f136506
Mazeworks <msk312508@gmail.com>
2025-10-29 16:59:43 +0530

    Work order

--- erp_custom/erp_custom/overrides/work_order.py
+++ erp_custom/erp_custom/overrides/work_order.py
@@ -1,17 +1,10 @@
 import json
 import frappe
 from frappe import _
-from dateutil.relativedelta import relativedelta
-from erpnext.manufacturing.doctype.work_order.work_order import get_template_rm_item
-from erpnext.stock.doctype.item.item import get_item_defaults, validate_end_of_life
-from erpnext.manufacturing.doctype.work_order.work_order import get_item_details
-from erpnext.manufacturing.doctype.work_order_item.work_order_item import WorkOrderItem
-from erpnext.manufacturing.doctype.bom.bom import (
-	get_bom_item_rate,
-	get_bom_items_as_dict,
-	validate_bom_no,
-)
-from frappe.utils import cint, flt, nowdate
+from erpnext.manufacturing.doctype.work_order.work_order import get_template_rm_item, get_item_details
+from erpnext.stock.doctype.item.item import get_item_defaults
+from erpnext.manufacturing.doctype.bom.bom import get_bom_item_rate
+from frappe.utils import cint, flt
 
 @frappe.whitelist()
 def make_work_order(bom_no, item, qty=0, project=None, variant_items=None, use_multi_level_bom=None):
@@ -23,8 +16,10 @@ def make_work_order(bom_no, item, qty=0, project=None, variant_items=None, use_m
 	if not frappe.has_permission("Work Order", "write"):
 		frappe.throw(_("Not permitted"), frappe.PermissionError)
 
+	# Fetch default item details
 	item_details = get_item_details(item, project)
 
+	# Create Work Order doc
 	wo_doc = frappe.new_doc("Work Order")
 	wo_doc.production_item = item
 	wo_doc.update(item_details)
@@ -35,20 +30,20 @@ def make_work_order(bom_no, item, qty=0, project=None, variant_items=None, use_m
 		wo_doc.qty = flt(qty)
 		wo_doc.get_items_and_operations_from_bom()
 
-		# Fetch the 3 custom values from BOM Item for this item
-		bom_item = frappe.db.get_value(
+		bom_items = frappe.get_all(
 			"BOM Item",
-			{"parent": bom_no, "item_code": item},
-			["custom_length", "custom_width", "custom_thickness"],
-			as_dict=True,
+			filters={"parent": bom_no},
+			fields=["item_code", "custom_length", "custom_width", "custom_thickness"]
 		)
 
-		if bom_item:
-			for req_item in wo_doc.required_items:
-				if req_item.item_code == item:
-					req_item.custom_length = bom_item.custom_length or 0
-					req_item.custom_width = bom_item.custom_width or 0
-					req_item.custom_thickness = bom_item.custom_thickness or 0
+		bom_map = {bi.item_code: bi for bi in bom_items}
+
+		for req_item in wo_doc.required_items:
+			bi = bom_map.get(req_item.item_code)
+			if bi:
+				req_item.custom_length = bi.custom_length or 0
+				req_item.custom_width = bi.custom_width or 0
+				req_item.custom_thickness = bi.custom_thickness or 0
 
 	if variant_items and not wo_doc.use_multi_level_bom:
 		add_variant_item(variant_items, wo_doc, bom_no, "required_items")
@@ -57,19 +52,20 @@ def make_work_order(bom_no, item, qty=0, project=None, variant_items=None, use_m
 
 
 def add_variant_item(variant_items, wo_doc, bom_no, table_name="items"):
+	"""
+	Add variant items into the Work Order dynamically.
+	"""
 	if isinstance(variant_items, str):
 		variant_items = json.loads(variant_items)
 
 	for item in variant_items:
-		args = frappe._dict(
-			{
-				"item_code": item.get("variant_item_code"),
-				"required_qty": item.get("qty"),
-				"qty": item.get("qty"),
-				"source_warehouse": item.get("source_warehouse"),
-				"operation": item.get("operation"),
-			}
-		)
+		args = frappe._dict({
+			"item_code": item.get("variant_item_code"),
+			"required_qty": item.get("qty"),
+			"qty": item.get("qty"),
+			"source_warehouse": item.get("source_warehouse"),
+			"operation": item.get("operation"),
+		})
 
 		bom_doc = frappe.get_cached_doc("BOM", bom_no)
 		item_data = get_item_details(args.item_code, skip_bom_info=True)
@@ -100,6 +96,7 @@ def add_variant_item(variant_items, wo_doc, bom_no, table_name="items"):
 			if table_name == "required_items"
 			else None
 		)
+
 		if existing_row:
 			existing_row.update(args)
 		else:
