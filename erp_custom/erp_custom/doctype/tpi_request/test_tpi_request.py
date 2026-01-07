ebcfde330db0ed65fb2586c52e25d20ac24d1db5
kponsakthivel <kponsakthivel@gmail.com>
2026-01-07 19:37:47 +0530

    Masters

---
+++ erp_custom/erp_custom/doctype/tpi_request/test_tpi_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, maze and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTPIRequest(FrappeTestCase):
+	pass
---
+++ erp_custom/erp_custom/doctype/tpi_request/tpi_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, maze and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TPIRequest(Document):
+	pass
