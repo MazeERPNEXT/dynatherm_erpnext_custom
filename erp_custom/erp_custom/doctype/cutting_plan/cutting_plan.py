49b96485378f10844f5e0e4db763b3b1f4abf31b
Mazeworks <msk312508@gmail.com>
2026-03-18 12:55:20 +0530

    Estimate & BOM Item

--- erp_custom/erp_custom/doctype/cutting_plan/cutting_plan.py
+++ erp_custom/erp_custom/doctype/cutting_plan/cutting_plan.py
@@ -29,12 +29,17 @@ def make_material_request(cutting_plan):
     mr.material_request_type = "Material Transfer"
     mr.schedule_date = cp.date
 
-    # 🔹 Use Correct Child Table Fieldname Here
+    # 🔹 Use Correct Child Table Fieldname Here (MR ->CP)
     for row in cp.cutting_plan_item:
         mr.append("items", {
             "item_code": row.item_code,
             "qty": row.qty if hasattr(row, "qty") else 1,
-            "schedule_date": cp.date
+            "schedule_date": cp.date,
+           
+            # Parent → Child mapping
+            "custom_length": cp.length,
+            "custom_width": cp.width,
+            "custom_thickness": cp.thickness,
         })
 
     mr.insert(ignore_permissions=True)
