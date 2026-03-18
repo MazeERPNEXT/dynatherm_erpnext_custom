990a4b58b7ff8848c5f2b3da3549eb810b53dcc2
Mazeworks <msk312508@gmail.com>
2026-03-18 16:23:03 +0530

    Estimate & Cutting Plan

--- erp_custom/erp_custom/doctype/cutting_plan/cutting_plan.py
+++ erp_custom/erp_custom/doctype/cutting_plan/cutting_plan.py
@@ -7,7 +7,6 @@
 
 # class CuttingPlan(Document):
 # 	pass
-
 import frappe
 from frappe.model.document import Document
 
@@ -24,19 +23,29 @@ def make_material_request(cutting_plan):
     if cp.docstatus != 1:
         frappe.throw("Cutting Plan must be Submitted")
 
+    # 🔹 Check existing Material Request using Cutting Plan number
+    existing_mr = frappe.db.get_value(
+        "Material Request Item",
+        {"custom_cutting_plan_no": cp.name},
+        "parent"
+    )
+
+    if existing_mr:
+        return existing_mr
+
+    # 🔹 Create new Material Request
     mr = frappe.new_doc("Material Request")
     mr.material_request_type = "Purchase"
     mr.schedule_date = cp.date
 
-    # Loop Cutting Plan Plate Details (Child Table)
+    # Loop Cutting Plan Plate Details
     for row in cp.cutting_plan_plate_details:
         mr.append("items", {
+            "custom_cutting_plan_no": cp.name,
             "item_code": row.item_code,
             "qty": row.qty or 1,
             "uom": row.uom,
             "schedule_date": cp.date,
-
-            # Child → Child Mapping
             "custom_length": row.length,
             "custom_width": row.width,
             "custom_thickness": row.thickness,
