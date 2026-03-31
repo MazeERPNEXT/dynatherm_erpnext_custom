09a4e28f2d6e899e45754030bd2cbb3ae6ac25fb
Mazeworks <msk312508@gmail.com>
2026-03-31 13:00:23 +0530

    Corrections

--- erp_custom/erp_custom/overrides/request_for_quotation.py
+++ erp_custom/erp_custom/overrides/request_for_quotation.py
@@ -58,6 +58,7 @@ def send_email_background(doc, method=None):
     # -------------------------------------------------
     header_html = """
         <th>Item Code</th>
+        <th>Brand</th>
         <th>Schedule Date</th>
         <th>Qty</th>
         <th>UOM</th>
@@ -83,6 +84,7 @@ def send_email_background(doc, method=None):
         row = f"""
         <tr>
             <td>{d.item_code or ""}</td>
+            <td>{d.custom_item_brand or ""}</td>
             <td>{d.schedule_date or ""}</td>
             <td>{d.qty or ""}</td>
             <td>{d.uom or ""}</td>
