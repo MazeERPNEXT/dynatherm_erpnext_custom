d54355971e100a7dbcf0c077f6b77f3544277343
Mazeworks <msk312508@gmail.com>
2026-04-08 13:39:56 +0530

    SO to Projects

--- erp_custom/erp_custom/doctype/cutting_plan/cutting_plan_dashboard.py
+++ erp_custom/erp_custom/doctype/cutting_plan/cutting_plan_dashboard.py
@@ -3,14 +3,47 @@
 
 # def get_data():
 # 	return {
-# 		"fieldname": "prevdoc_docname",
+# 		"fieldname": "project",
 # 		"non_standard_fieldnames": {
 # 			"Auto Repeat": "reference_document",
 # 		},
 # 		"transactions": [
-# 			{"label": _("Customer Request For Quotation"), "items": ["Customer Request For Quotation"]},
-# 			{"label": _("Estimate"), "items": ["Estimate"]},
-# 			{"label": _("Quotation"), "items": ["Quotation"]},
+# 			# {"label": _("Customer Request For Quotation"), "items": ["Customer Request For Quotation"]},
+# 			# {"label": _("Estimate"), "items": ["Estimate"]},
+# 			# {"label": _("Quotation"), "items": ["Quotation"]},
 #             {"label": _("Project"), "items": ["Project"]},
 # 		],
 # 	}
+
+
+
+# from frappe import _
+
+# def get_data():
+#     return {
+#         "fieldname": "project",
+#         "transactions": [
+#             {
+#                 "label": _("Project"),
+#                 "items": ["Project"]
+#             }
+#         ],
+#     }
+
+
+from frappe import _
+
+def get_data():
+    return {
+        "fieldname": "project",
+        # "non_standard_fieldnames": {
+        #     # "Auto Repeat": "reference_document",
+        #     "Project": "name"
+        # },
+        "transactions": [
+            {
+                "label": _("Project"),
+                "items": ["Project"]
+            }
+        ],
+    }
